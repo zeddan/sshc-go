baa06aef0f9423797b221623bf8d52a9e9f66043
zeddan <robin.saaf@gmail.com>
2019-11-08 17:51:08 +0100

    Add connection to ssh server

--- sshc-go.go
+++ sshc-go.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -21,9 +23,12 @@ func connect() {
 	fmt.Println("Select instance:")
 	fmt.Println()
 
+	var servers []string
+
 	scanner := bufio.NewScanner(f)
 	i := 1
 	for scanner.Scan() {
+		servers = append(servers, scanner.Text())
 		s := strings.Split(scanner.Text(), ",")
 		fmt.Printf("%d) %s\n", i, s[0])
 		i++
@@ -33,7 +38,23 @@ func connect() {
 	fmt.Print("> ")
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	text = strings.TrimSuffix(text, "\n")
+
+	choice, err := strconv.Atoi(text)
+	check(err)
+	choice--
+
+	server := strings.Split(servers[choice], ",")
+
+	fmt.Printf("ssh %s@%s\n", server[1], server[2])
+
+	cmd := exec.Command("ssh", fmt.Sprintf("%s@%s", server[1], server[2]))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	error := cmd.Run()
+	check(error)
+
 }
 
 func main() {
