3104aaf1e5f1f7214c4eb2e093f0be58ef398224
zeddan <robin.saaf@gmail.com>
2019-11-10 21:17:53 +0100

    Add server listing

--- sshc-go.go
+++ sshc-go.go
@@ -57,6 +57,58 @@ func connect() {
 
 }
 
+func findWidths(servers [][]string) []string {
+	var widths []string
+	longest := 0
+	for i := range servers {
+		length := len(servers[i][0])
+		if length > longest {
+			longest = length
+		}
+	}
+	widths = append(widths, strconv.Itoa(longest+2))
+	longest = 0
+	for i := range servers {
+		length := len(servers[i][1])
+		if length > longest {
+			longest = length
+		}
+	}
+	widths = append(widths, strconv.Itoa(longest+2))
+	return widths
+}
+
+func prettyPrint(name string, user string, ip string, widths []string) {
+	col1, _ := strconv.Atoi(widths[0])
+	col2, _ := strconv.Atoi(widths[1])
+	x := fmt.Sprintf("%-"+fmt.Sprintf("%d", col1)+"s %-"+fmt.Sprintf("%d", col2)+"s %s", name, user, ip)
+	fmt.Println(x)
+}
+
+func list() {
+	f, err := os.Open("/Users/zeddan/.config/sshc/instances")
+	check(err)
+	defer f.Close()
+
+	var servers [][]string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		server := strings.Split(scanner.Text(), ",")
+		servers = append(servers, server)
+	}
+
+	widths := findWidths(servers)
+	prettyPrint("NAME", "USER", "IP", widths)
+	for i := range servers {
+		name := servers[i][0]
+		user := servers[i][1]
+		ip := servers[i][2]
+		prettyPrint(name, user, ip, widths)
+	}
+}
+
 func main() {
-	connect()
+	//connect()
+	list()
 }
